7a55ad022c8f5f8da5507538df682fff276c8ce9
Wayne Thompson <wayne_thompson@hotmail.com>
2018-04-23 16:50:38 +1000

    Added vincere exceptions

---
+++ VincereSharp/Model/ResponseError.cs
@@ -0,0 +1,25 @@
+﻿using Newtonsoft.Json;
+
+namespace VincereSharp
+{
+    public class ResponseError
+    {
+        [JsonProperty("message")]
+        public string Message { get; set; }
+
+        [JsonProperty("status")]
+        public string Status { get; set; }
+
+        [JsonProperty("httpStatus")]
+        public string HttpStatus { get; set; }
+
+        [JsonProperty("errorCode")]
+        public string ErrorCode { get; set; }
+
+        [JsonProperty("errorId")]
+        public string ErrorId { get; set; }
+
+        [JsonProperty("errors")]
+        public string[] Errors { get; set; }
+    }
+}
